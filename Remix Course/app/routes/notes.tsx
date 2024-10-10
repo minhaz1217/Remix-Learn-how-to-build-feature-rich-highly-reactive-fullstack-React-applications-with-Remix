b7387f4805646f4d38115269e21e6d7cb80d4362
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-10 15:35:47 +0600

    lesson 22 - working with form and action

--- Remix Course/app/routes/notes.tsx
+++ Remix Course/app/routes/notes.tsx
@@ -1,4 +1,6 @@
+import { ActionFunction, ActionFunctionArgs, redirect } from "@remix-run/node";
 import NewNote, { links as newNoteLinks } from "~/components/NewNote/NewNote";
+import { getStoredNotes, storeNotes } from "~/data/notes";
 
 export default function Notes() {
   return (
@@ -6,6 +8,18 @@ export default function Notes() {
       <NewNote />
     </main>
   );
+}
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const noteData = Object.fromEntries(formData);
+  noteData.id = new Date().toISOString();
+
+  const existingData = await getStoredNotes();
+  const updatedNotes = existingData.concat(noteData);
+  await storeNotes(updatedNotes);
+  // return redirect("/notes"); // if needed
+  return null;
 }
 
 export function links() {
