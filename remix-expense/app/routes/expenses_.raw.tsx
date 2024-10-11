751b1a844e40d542574c716b51035ffd8d5b6239
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-12 02:06:25 +0600

    lesson 53 - resource route and splat route

--- remix-expense/app/routes/expenses_.raw.tsx
+++ remix-expense/app/routes/expenses_.raw.tsx
@@ -1,5 +1,17 @@
-const ExpensesRawPage = () => {
-  return <h1>Raw</h1>;
-};
-
-export default ExpensesRawPage;
+const DUMMY_EXPENSE = [
+  {
+    id: "e1",
+    title: "First Expense",
+    amount: 10,
+    date: new Date().toISOString(),
+  },
+  {
+    id: "e2",
+    title: "Second Expense",
+    amount: 15,
+    date: new Date().toISOString(),
+  },
+];
+export function loader() {
+  return DUMMY_EXPENSE;
+}
