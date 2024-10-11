8fafc340c10b7022aeedce76020ebd9d6573e75c
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-12 05:38:57 +0600

    lesson 70 - updating data

--- remix-expense/app/components/expenses/ExpenseForm.jsx
+++ remix-expense/app/components/expenses/ExpenseForm.jsx
@@ -1,16 +1,42 @@
-import { Link, useActionData, useNavigation } from "@remix-run/react";
+import {
+  Link,
+  useActionData,
+  useNavigation,
+  useLoaderData,
+} from "@remix-run/react";
 import { Form } from "@remix-run/react";
 function ExpenseForm() {
   const validationErrors = useActionData();
   const navigation = useNavigation();
+
+  const expenseData = useLoaderData();
   const today = new Date().toISOString().slice(0, 10); // yields something like 2023-09-10
   const isSubmitting = navigation.state !== "idle";
+
+  const defaultValues = expenseData
+    ? {
+        title: expenseData.title,
+        amount: expenseData.amount,
+        date: expenseData.date,
+      }
+    : {
+        title: "",
+        amount: "",
+        date: "",
+      };
 
   return (
     <Form method="post" className="form" id="expense-form">
       <p>
         <label htmlFor="title">Expense Title</label>
-        <input type="text" id="title" name="title" required maxLength={30} />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          required
+          maxLength={30}
+          defaultValue={defaultValues.title}
+        />
       </p>
 
       <div className="form-row">
@@ -22,12 +48,22 @@ function ExpenseForm() {
             name="amount"
             min="0"
             step="0.01"
+            defaultValue={defaultValues.amount}
             required
           />
         </p>
         <p>
           <label htmlFor="date">Date</label>
-          <input type="date" id="date" name="date" max={today} required />
+          <input
+            type="date"
+            id="date"
+            name="date"
+            max={today}
+            required
+            defaultValue={
+              defaultValues.date ? defaultValues.date.slice(0, 10) : ""
+            }
+          />
         </p>
       </div>
       {validationErrors && (
--- remix-expense/app/components/expenses/ExpenseListItem.jsx
+++ remix-expense/app/components/expenses/ExpenseListItem.jsx
@@ -1,5 +1,6 @@
 import { Link } from "@remix-run/react";
 function ExpenseListItem({ id, title, amount }) {
+  console.debug("Expense Id", id);
   function deleteExpenseItemHandler() {
     // tbd
   }
@@ -12,7 +13,7 @@ function ExpenseListItem({ id, title, amount }) {
       </div>
       <menu className="expense-actions">
         <button onClick={deleteExpenseItemHandler}>Delete</button>
-        <Link to={id}>Edit</Link>
+        <Link to={`${id}`}>Edit</Link>
       </menu>
     </article>
   );
