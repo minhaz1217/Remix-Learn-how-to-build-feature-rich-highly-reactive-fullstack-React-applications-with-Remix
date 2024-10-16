1e618e6835f9f674c0f8d08b655aed00b11d1bd3
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-17 03:37:04 +0600

    lesson 76 - using useFetcher to make api calls

--- remix-expense/app/components/expenses/ExpenseListItem.jsx
+++ remix-expense/app/components/expenses/ExpenseListItem.jsx
@@ -1,10 +1,29 @@
-import { Form, Link } from "@remix-run/react";
+import { Form, Link, useFetcher, useSubmit } from "@remix-run/react";
 function ExpenseListItem({ id, title, amount }) {
+  const submit = useSubmit();
+  const fetcher = useFetcher();
+
   console.debug("Expense Id", id);
   function deleteExpenseItemHandler() {
-    // tbd
-  }
+    const proceed = confirm("Are you sure? Do you want to delete this item?");
+    // submit(null , {
+    //    method: "DELETE",
+    //    action : `/expense/${id}`
+    // });
 
+    if (!proceed) {
+      return;
+    }
+    fetcher.submit(null, {
+      method: "DELETE",
+      action: `/expense/${id}`,
+    });
+
+    console.debug("fetcher.data", fetcher.data);
+  }
+  if (fetcher.state !== "idle") {
+    return <article className="expense-item locked">Deleting...</article>;
+  }
   return (
     <article className="expense-item">
       <div>
@@ -12,10 +31,10 @@ function ExpenseListItem({ id, title, amount }) {
         <p className="expense-amount">${amount.toFixed(2)}</p>
       </div>
       <menu className="expense-actions">
-        {/* <button onClick={deleteExpenseItemHandler}>Delete</button> */}
-        <Form method="delete" action={`/expenses/${id}`}>
+        <button onClick={deleteExpenseItemHandler}>Delete</button>
+        {/* <Form method="delete" action={`/expenses/${id}`}>
           <button type="submit">Delete</button>
-        </Form>
+        </Form> */}
         <Link to={`${id}`}>Edit</Link>
       </menu>
     </article>
