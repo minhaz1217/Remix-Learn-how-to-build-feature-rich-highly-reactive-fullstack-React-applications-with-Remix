2b137310c71d88f8f74ce24ca987d1a1ad934e20
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-10-29 23:47:40 +0600

    lesson 75 - updating and deleting

--- remix-expense/app/routes/_app.expenses.$id.tsx
+++ remix-expense/app/routes/_app.expenses.$id.tsx
@@ -1,8 +1,13 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData, useNavigate } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { redirect, useLoaderData, useNavigate } from "@remix-run/react";
 import ExpenseForm from "~/components/expenses/ExpenseForm";
 import Modal from "~/components/util/Modal";
-import { getExpense } from "~/data/expenses.server";
+import {
+  deleteExpense,
+  getExpense,
+  updateExpense,
+} from "~/data/expenses.server";
+import { validateExpenseInput } from "~/data/validation.server";
 
 const ExpensesUpdatePage = () => {
   const navigate = useNavigate();
@@ -25,3 +30,26 @@ export default ExpensesUpdatePage;
 //   const expense = await getExpense(expenseId as string);
 //   return expense;
 // }
+
+export async function action({ request, params }: ActionFunctionArgs) {
+  const expenseId = params.id;
+  if (request.method === "PATCH") {
+    const formData = await request.formData();
+    const expenseData = Object.fromEntries(formData);
+    try {
+      validateExpenseInput(expenseData);
+
+      await updateExpense(expenseId?.toString() ?? "", expenseData as any);
+      return redirect("/expenses");
+    } catch (error: any) {
+      return error;
+    }
+  } else if (request.method === "DELETE") {
+    try {
+      await deleteExpense(expenseId?.toString() ?? "");
+    } catch (error: any) {
+      return error;
+    }
+    return redirect("/expenses");
+  }
+}
