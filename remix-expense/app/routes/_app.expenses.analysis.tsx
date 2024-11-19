10ce1a5f05cf1524bbb28aac9763a51b37187d7e
Minhazul Hayat Khan <minhaz1217@gmail.com>
2024-11-19 23:20:36 +0600

    lesson 83 - fixed the analysis page

--- remix-expense/app/routes/_app.expenses.analysis.tsx
+++ remix-expense/app/routes/_app.expenses.analysis.tsx
@@ -1,5 +1,8 @@
+import { json, useLoaderData, useRouteError } from "@remix-run/react";
 import Chart from "~/components/expenses/Chart";
 import ExpenseStatistics from "~/components/expenses/ExpenseStatistics";
+import { getExpense, getExpenses } from "~/data/expenses.server";
+import Error from "~/components/util/Error";
 
 const DUMMY_EXPENSE = [
   {
@@ -16,12 +19,39 @@ const DUMMY_EXPENSE = [
   },
 ];
 const ExpensesAnalysisPage = () => {
+  const expenses = useLoaderData();
   return (
     <main>
-      <Chart expenses={DUMMY_EXPENSE} />
-      <ExpenseStatistics expenses={DUMMY_EXPENSE} />
+      <Chart expenses={expenses} />
+      <ExpenseStatistics expenses={expenses} />
     </main>
   );
 };
 
+export async function loader() {
+  const expenses = await getExpenses();
+  if (!expenses || expenses.length === 0) {
+    throw json(
+      {
+        message: "No expenses found",
+      },
+      {
+        status: 404,
+      }
+    );
+  }
+  return expenses;
+}
+
 export default ExpensesAnalysisPage;
+
+export function ErrorBoundary() {
+  const error = useRouteError();
+  return (
+    <main>
+      <Error title={error?.statusText}>
+        <p>{error.data?.message || "Something went wrong"}</p>
+      </Error>
+    </main>
+  );
+}
